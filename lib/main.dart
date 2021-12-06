d6ccd30d643767266019e01d6f630e1c31061c89
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-12-06 15:20:18 +0500

    Hassan ~ primary swatch set

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,29 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  final MaterialColor primaryColor = const MaterialColor(
+    0xff000000,
+    <int, Color>{
+      50: Color(0xff000000),
+      100: Color(0xff000000),
+      200: Color(0xff000000),
+      300: Color(0xff000000),
+      400: Color(0xff000000),
+      500: Color(0xff000000),
+      600: Color(0xff000000),
+      700: Color(0xff000000),
+      800: Color(0xff000000),
+      900: Color(0xff000000),
+    },
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Baranh',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: primaryColor,
         textTheme: GoogleFonts.sourceSansProTextTheme(
           Theme.of(context).textTheme,
         ),
