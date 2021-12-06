b9e4ab4baa691bf7114630c7f53d0da2e304fec4
unknown <naqeeb9a@gmail.com>
2021-12-06 14:52:08 +0500

    naqeeb -- in progress

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,6 +5,7 @@ import 'package:baranh/widgets/drawer.dart';
 import 'package:baranh/widgets/essential_widgets.dart';
 import 'package:baranh/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var hintText = "mm/dd/yy";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,44 +46,61 @@ class _HomeState extends State<Home> {
               heightBox(context, 0.02),
               inputFieldsHome(context, "Reservation Number:", "Ex:Res.00042"),
               heightBox(context, 0.02),
-              inputFieldsHome(context, "Select Date:", "mm/dd/yy", check: true),
+              inputFieldsHome(context, "Select Date:", hintText, check: true),
               heightBox(context, 0.03),
-              coloredButton(context, "SEARCH", myOrange)
+              coloredButton(context, "SEARCH", myOrange, fontsize: 0.038)
             ],
           ),
         ),
       ),
     );
   }
-}
 
-Widget inputFieldsHome(context, text1, hintText1, {check = false}) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      text(context, text1, 0.04, myWhite),
-      Container(
-        color: myWhite,
-        padding: EdgeInsets.symmetric(horizontal: dynamicWidth(context, 0.04)),
-        child: (check == true)
-            ? Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: dynamicWidth(context, 0.5),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                          hintText: hintText1, fillColor: myWhite),
-                    ),
+  Widget inputFieldsHome(context, text1, hintText1, {check = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        text(context, text1, 0.04, myWhite),
+        Container(
+          color: myWhite,
+          padding:
+              EdgeInsets.symmetric(horizontal: dynamicWidth(context, 0.04)),
+          child: (check == true)
+              ? InkWell(
+                  onTap: () async {
+                    var newTime = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1999, 1, 1),
+                        lastDate: DateTime.now());
+                    setState(() {
+                      hintText = DateFormat.yMMMd().format(newTime!).toString();
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: dynamicWidth(context, 0.5),
+                        child: TextFormField(
+                          readOnly: true,
+                          decoration: InputDecoration(
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              hintText: hintText,
+                              fillColor: myWhite),
+                        ),
+                      ),
+                      const Icon(Icons.calendar_today_outlined)
+                    ],
                   ),
-                  const Icon(Icons.calendar_today_outlined)
-                ],
-              )
-            : TextFormField(
-                decoration:
-                    InputDecoration(hintText: hintText1, fillColor: myWhite),
-              ),
-      )
-    ],
-  );
+                )
+              : TextFormField(
+                  decoration:
+                      InputDecoration(hintText: hintText1, fillColor: myWhite),
+                ),
+        )
+      ],
+    );
+  }
 }
--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -3,30 +3,23 @@ import 'package:flutter/material.dart';
 
 import '../utils/config.dart';
 
-Widget coloredButton(context, text, color, {function = "", width = ""}) {
-  return Material(
-    borderRadius: BorderRadius.circular(
-      dynamicWidth(context, .4),
-    ),
-    child: GestureDetector(
-      onTap: function == "" ? () {} : function,
-      child: Container(
-        width: width == "" ? dynamicWidth(context, 1) : width,
-        height: dynamicHeight(context, .056),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(
-            dynamicWidth(context, .02),
-          ),
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: myWhite,
-              fontWeight: FontWeight.bold,
-              fontSize: dynamicWidth(context, .054),
-            ),
+Widget coloredButton(context, text, color,
+    {function = "", width = "", fontsize = 0.05}) {
+  return GestureDetector(
+    onTap: function == "" ? () {} : function,
+    child: Container(
+      width: width == "" ? dynamicWidth(context, 1) : width,
+      height: dynamicHeight(context, .056),
+      decoration: BoxDecoration(
+        color: color,
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: myWhite,
+            fontWeight: FontWeight.bold,
+            fontSize: dynamicWidth(context, fontsize),
           ),
         ),
       ),
